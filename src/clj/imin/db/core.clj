1a999125a2187fb71d9fd29f781d3cc18a5eb798
Zola Zhou <zolazhou@gmail.com>
2016-09-23 16:32:56 +0800

    Remove test code

--- src/clj/imin/db/core.clj
+++ src/clj/imin/db/core.clj
@@ -4,10 +4,3 @@
 (defonce ^:dynamic *db* (conman/connect! {:jdbc-url "jdbc:h2:./h2.db"}))
 
 (conman/bind-connection *db* "sql/queries.sql")
-
-
-
-(create-user! {:username "zola"
-               :email    "zolazhou@gmail.com"
-               :alipay   "zolazhou@gmail.com"})
-
